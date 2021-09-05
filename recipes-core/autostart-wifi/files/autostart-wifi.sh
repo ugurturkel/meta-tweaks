0bcbf51e207ed73149ab237e870659ec7e03375e
Uğur Türkel <48327110+ugurturkel@users.noreply.github.com>
2021-09-05 17:48:09 +0300

    update autostart-wifi.sh

--- recipes-core/autostart-wifi/files/autostart-wifi.sh
+++ recipes-core/autostart-wifi/files/autostart-wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rfkill unblock all
-wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant.conf
 ifconfig wlan0 192.168.1.166
 route add default gw 192.168.1.1
+wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant.conf
 : exit 0
